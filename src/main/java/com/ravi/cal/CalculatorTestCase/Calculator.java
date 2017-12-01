612bcad993bde63ca8f8c4cfbe9a1e132218fa45
devops <devopsguy9@gmail.com>
2017-12-01 06:43:51 +0530

    Java unit test case demonstration with Calculator

---
+++ src/main/java/com/ravi/cal/CalculatorTestCase/Calculator.java
@@ -0,0 +1,17 @@
+package com.ravi.cal.CalculatorTestCase;
+
+public class Calculator {
+    
+	public long add(long first, long second) {
+        return first + second;
+    }
+    
+	public long subtract(long first, long second) {
+        return first - second;
+    }
+    
+	public long multiply(long first, long second) {
+        return first * second;
+    }
+    
+}
